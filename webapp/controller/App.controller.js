3f07ac4d39f242b6ba22774617e97a6f8601a36f
ThaisHerminia <thaisperruolo@gmail.com>
2022-05-22 20:21:54 +0000

    primera confirmacion en desarrollo

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("tareathais.actividad3.controller.controller.App", {
+        onInit() {
+        }
+      });
+    }
+  );
+  
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"tarea_thais/actividad3/test/unit/controller/vista1.controller"
+], function () {
+	"use strict";
+});
